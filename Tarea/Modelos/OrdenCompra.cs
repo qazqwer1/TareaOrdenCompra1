f140bb95a77d0c73f8b4540a56baffca55c032fc
Giox <elbartoinc@gmail.com>
2025-03-28 16:47:43 -0300

    Se agrega etiqueta required

--- Tarea/Modelos/OrdenCompra.cs
+++ Tarea/Modelos/OrdenCompra.cs
@@ -1,12 +1,22 @@
-﻿namespace Tarea.Modelos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Tarea.Modelos
 {
     public class OrdenCompra
     {
         public int Id { get; set; }
+
+        [Required]
         public string Cliente { get; set; } = string.Empty;
+
+
         public DateTime FechaCreacion { get; set; } = DateTime.UtcNow;
+
+
         public decimal Total { get; set; }
 
         public List<OrdenProducto> Productos { get; set; } = new();
     }
 }
+
+
--- Tarea/Modelos/Producto.cs
+++ Tarea/Modelos/Producto.cs
@@ -1,9 +1,16 @@
-﻿namespace Tarea.Modelos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Tarea.Modelos
 {
     public class Producto
     {
         public int Id { get; set; }
+
+        [Required]
         public string Nombre { get; set; } = string.Empty;
+
+        [Required]
+        [Range(0.01, double.MaxValue, ErrorMessage = "El precio debe ser mayor que cero")]
         public decimal Precio { get; set; }
     }
 }
